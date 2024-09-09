a81f421f46b0d33b7e38210d5a3e948384b30c40
akira.nieda <akira.nieda@GX2191.local>
2024-09-09 19:47:20 +0900

    update

--- packages/client/src/components/NftUploader/NftUploader.jsx
+++ packages/client/src/components/NftUploader/NftUploader.jsx
@@ -5,7 +5,13 @@ import "./NftUploader.css";
 import {useEffect, useState} from "react";
 
 const NftUploader = () => {
-    const checkIfWalletIsConnected = () => {
+    /*
+    * ユーザーのウォレットアドレスを格納するために使用する状態変数を定義します。
+    */
+    const [currentAccount, setCurrentAccount] = useState("");
+    /*この段階で currentAccount の中身は空*/
+    console.log("currentAccount: ", currentAccount);
+    const checkIfWalletIsConnected = async () => {
         /*
         * ユーザーが MetaMask を持っているか確認します。
         */
@@ -15,12 +21,38 @@ const NftUploader = () => {
         } else {
             console.log("We have the ethereum object", ethereum);
         }
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+        if (accounts.length !== 0) {
+            const account = accounts[0];
+            console.log("Found an authorized account:", account);
+            setCurrentAccount(account);
+        } else {
+            console.log("No authorized account found");
+        }
     };
-    const connectWallet = () =>{
+    const connectWallet = async () =>{
+        try {
+            const { ethereum } = window;
+            if (!ethereum){
+                alert("Connect your MetaMask account.");
+                return;
+            }
+            const permissions = await ethereum.request({ method: 'eth_requestAccounts' });
+            console.log("Connected", permissions[0]);
+            setCurrentAccount(permissions[0]);
+        } catch (err) {
+            console.error(err);
+        }
     };
     const renderNotConnectedContainer = () => (
         <button onClick={connectWallet} className="cta-button connect-wallet-button">
             Connect to Wallet
+        </button>
+    );
+
+    const renderConnectedContainer = () => (
+        <button className="cta-button cunnect-wallet-button">
+            If you choose image, you can mint your NFT
         </button>
     );
     /*
@@ -30,10 +62,17 @@ const NftUploader = () => {
         checkIfWalletIsConnected();
     }, []);
 
+
   return (
     <div className="outerBox">
       <div className="title">
         <h2>NFTアップローダー</h2>
+          <p className="sub-text">あなただけの特別な NFT を Mint しよう💫</p>
+          {currentAccount === "" ? (
+              renderNotConnectedContainer()
+          ) : (
+              renderConnectedContainer()
+          )}
       </div>
       <div className="nftUplodeBox">
         <div className="imageLogoAndText">
