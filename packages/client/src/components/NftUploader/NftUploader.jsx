4c2e7f3eb582f6b88d3777693796f3f873f8385f
akira.nieda <akira.nieda@GX2191.local>
2024-09-17 15:37:01 +0900

    could upload files via lighthouse

--- packages/client/src/components/NftUploader/NftUploader.jsx
+++ packages/client/src/components/NftUploader/NftUploader.jsx
@@ -3,15 +3,23 @@ import React from "react";
 import ImageLogo from "./image.svg";
 import "./NftUploader.css";
 import {useEffect, useState} from "react";
-import ethers from "ethers";
+//import ethers from "ethers";
 import Web3Mint from "../../utils/Web3Mint.json";
 import {Web3Storage} from "web3.storage";
+import {PinataSDK} from "pinata-web3";
+import lighthouse from "@lighthouse-web3/sdk";
+import {LIGHT_HOUSE_API_KEY, PINATA_API_KEY, PINATA_JWT, PINATA_GATEWAY} from "./.config.ts";
+//import fs from "fs";
+const ethers = require("ethers");
 
 const NftUploader = () => {
     /*
     * ユーザーのウォレットアドレスを格納するために使用する状態変数を定義します。
     */
     const [currentAccount, setCurrentAccount] = useState("");
+
+    const [preview, setPreview] = useState("")
+
     /*この段階で currentAccount の中身は空*/
     console.log("currentAccount: ", currentAccount);
     const checkIfWalletIsConnected = async () => {
@@ -46,32 +54,32 @@ const NftUploader = () => {
         } catch (err) {
             console.error(err);
         }
-        const askContractToMintNft = async (ipfs) => {
-            const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-            try {
-                const { ethereum } = window;
-                if (ethereum) {
-                    const provider = new ethers.providers.Web3Provider(ethereum);
-                    const signer = provider.getSigner();
-                    const connectedContract = new ethers.Contract(
-                        CONTRACT_ADDRESS,
-                        Web3Mint.abi,
-                        signer
-                    );
-                    console.log("Going to pop wallet now to pay gas...");
-                    let nftTxn = await connectedContract.mintIpfsNFT("sample",ipfs);
-                    console.log("Mining...please wait.");
-                    await nftTxn.wait();
-                    console.log(
-                        `Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`
-                    );
-                } else {
-                    console.log("Ethereum object doesn't exist!");
-                }
-            } catch (error) {
-                console.log(error);
+    };
+    const askContractToMintNft = async (ipfs) => {
+        const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+        try {
+            const { ethereum } = window;
+            if (ethereum) {
+                const provider = new ethers.BrowserProvider(ethereum);
+                const signer = await provider.getSigner();
+                const connectedContract = new ethers.Contract(
+                    CONTRACT_ADDRESS,
+                    Web3Mint,
+                    signer
+                );
+                console.log("Going to pop wallet now to pay gas...");
+                let nftTxn = await connectedContract.mintIpfsNFT("sample", ipfs);
+                console.log("Mining...please wait.");
+                await nftTxn.wait();
+                console.log(
+                    `Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`
+                );
+            } else {
+                console.log("Ethereum object doesn't exist!");
             }
-        };
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     const renderNotConnectedContainer = () => (
@@ -92,21 +100,47 @@ const NftUploader = () => {
         checkIfWalletIsConnected();
     }, []);
 
-const imageToNFT = async (e) => {
-    const client = new Web3Storage({ token: API_KEY })
-    const image = e.target
-    console.log(image)
-    const rootCid = await client.put(image.files, {
-        name: 'experiment',
-        maxRetries: 3
-    })
-    const res = await client.get(rootCid) // Web3Response
-    const files = await res.files() // Web3File[]
-    for (const file of files) {
-        console.log("file.cid:",file.cid)
-        askContractToMintNft(file.cid)
+
+    //ここで画像をきちんとIPFSにあげないと行けない
+    //ここがネック
+    const imageToNFT = async (e) => {
+        /*
+        const pinata = new PinataSDK({
+            pinataJwt : PINATA_JWT,
+            pinataGateway : PINATA_GATEWAY
+        });
+         */
+        //const image = e.target.files[0];
+        const image = e.target.files;
+
+        const progressCallback = (progressData) => {
+            let percentageDone =
+                100 - (progressData?.total / progressData?.uploaded)?.toFixed(2)
+            console.log(percentageDone)
+        }
+
+        //const uploadRes = await lighthouse.upload(`${image}`, LIGHT_HOUSE_API_KEY);
+        //console.log(uploadRes);
+        //await fs.writeFileSync(`/images/${image}`, image);
+        const output = await lighthouse.upload(image, LIGHT_HOUSE_API_KEY, null, progressCallback)
+        console.log('File Status:', output)
+
+        const imageURL = window.URL.createObjectURL(image);
+        setPreview(imageURL);
+
+        console.log(e);
+        //const file = new File(["test"], image, {type: "image/png"});
+        //const upload = await pinata.upload.file(file);
+        //const file = new File(["experiment"], image, { type: "image/png" });
+        //const upload = await pinata.upload.file(file);
+        //const upload = await pinata.upload.url(preview);
+        //const IpfsHash = upload.IpfsHash
+        //console.log(IpfsHash);
+
+        //const res = await pinata.gateways.get(IpfsHash);
+        //console.log(res);
+        //askContractToMintNft(res);
     }
-}
 
   return (
     <div className="outerBox">
@@ -121,7 +155,7 @@ const imageToNFT = async (e) => {
       </div>
       <div className="nftUplodeBox">
         <div className="imageLogoAndText">
-          <img src={ImageLogo} alt="imagelogo" />
+          <img src={preview} alt="imagelogo" />
           <p>ここにドラッグ＆ドロップしてね</p>
         </div>
         <input
